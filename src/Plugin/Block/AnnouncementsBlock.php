0340d7bf0b1103b55b420a0588eeb115c981bad4
Mads Nørgaard <madsnorgaard@live.dk>
2023-05-15 16:01:12 +0200

    Add access check and minor changes

--- src/Plugin/Block/AnnouncementsBlock.php
+++ src/Plugin/Block/AnnouncementsBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -34,6 +35,13 @@ class AnnouncementsBlock extends BlockBase implements ContainerFactoryPluginInte
   protected $cacheTagsInvalidator;
 
   /**
+   * Configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * Constructs a new AnnouncementsBlock object.
    *
    * @param array $configuration
@@ -46,49 +54,48 @@ class AnnouncementsBlock extends BlockBase implements ContainerFactoryPluginInte
    *   Announcement entity view builder class.
    * @param \Drupal\Core\Cache\CacheTagsInvalidatorInterface $cache_tags_invalidator
    *   Cache tags invalidator service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Configuration factory.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, CacheTagsInvalidatorInterface $cache_tags_invalidator) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, CacheTagsInvalidatorInterface $cache_tags_invalidator, ConfigFactoryInterface $config_factory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
     $this->cacheTagsInvalidator = $cache_tags_invalidator;
+    $this->configFactory = $config_factory;
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity_type.manager'), $container->get('cache_tags.invalidator'));
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity_type.manager'), $container->get('cache_tags.invalidator'), $container->get('config.factory'));
   }
 
-/**
- * {@inheritdoc}
- */
-public function build() {
-  $build = [];
-  $cache_tags = ['announcement_list'];
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $config = $this->configFactory->get('announcement.settings');
+    $cache_tags = [$config->get('cache_tags')];
 
-  // Check if the 'announcement' entity type exists.
-  if ($this->entityTypeManager->hasDefinition('announcement')) {
     $announcementStorage = $this->entityTypeManager->getStorage('announcement');
+    $query = $announcementStorage->getQuery();
+    $query->condition('status', TRUE);
+    $ids = $query->accessCheck(FALSE)->execute();
 
-    // Query and load the entities only if the entity type exists.
-    $ids = $announcementStorage->getQuery()->condition('status', TRUE)->execute();
     $entities = $announcementStorage->loadMultiple($ids);
 
-    // Check if entities are found.
-    if (!empty($entities)) {
-      $build['announcements'] = $this->entityTypeManager->getViewBuilder('announcement')->viewMultiple($entities, 'full');
+    $build['announcements'] = $this->entityTypeManager->getViewBuilder('announcement')->viewMultiple($entities);
 
-      foreach ($entities as $entity) {
-        $cache_tags = array_merge($cache_tags, $entity->getCacheTags());
-      }
+    foreach ($entities as $entity) {
+      $cache_tags = array_merge($cache_tags, $entity->getCacheTags());
     }
+
+    $this->addCacheableDependency($build, $cache_tags);
+
+    return $build;
   }
-
-  $this->addCacheableDependency($build, $cache_tags);
-
-  return $build;
-}
 
   /**
    * Adds cacheable dependency for the block.
@@ -99,7 +106,7 @@ public function build() {
    *   The cache tags array.
    */
   protected function addCacheableDependency(array &$build, array $cache_tags) {
+    $this->cacheTagsInvalidator->invalidateTags($cache_tags);
     $build['#cache']['tags'] = CacheTagsInvalidatorInterface::mergeTags($build['#cache']['tags'], $cache_tags);
   }
-
 }
